8ca8aec4cc4f5230b3c0e449da6583af3c33f053
Im2World <hi.programmer7@gmail.com>
2022-08-26 09:39:12 +0900

    11022_printing A + B(Buffered)2

---
+++ 03.반복문/11022_printing A + B(Buffered)2.java
@@ -0,0 +1,46 @@
+package num11022;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main2 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 
+		int caseNum = Integer.parseInt(br.readLine());
+ 
+		StringBuilder sb = new StringBuilder();
+ 
+		int a, b, sum;
+		
+		for (int i = 0; i < caseNum; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");    //("문자열","구분자") input
+			a = Integer.parseInt(st.nextToken());					//변수 값 갱신
+			b = Integer.parseInt(st.nextToken());
+			sum = a + b;
+			
+			//nextToken return type String
+			sb.append("Case #"+ (i+1)).append(": ").append(a + " + " + b + " " + "= " + sum).append("\n");		//입력된 숫자끼리 덧셈. 1 2 3 4 => 1 + 2 , 3 + 4
+		}
+		br.close();
+		System.out.println(sb);
+	}
+}
+
+
+/*
+5
+1 1
+2 3
+3 4
+9 8
+5 2
+Case #1: 1 + 1 = 2
+Case #2: 2 + 3 = 5
+Case #3: 3 + 4 = 7
+Case #4: 9 + 8 = 17
+Case #5: 5 + 2 = 7
+*/
